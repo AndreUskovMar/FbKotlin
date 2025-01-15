2dd2bbe18e9898352d7c4d52fcc791c85fb1ddbf
andrewuskov <andreuskov2211@yandex.ru>
2025-01-15 09:49:03 +0200

    Lesson 10. Authentication

--- app/src/main/java/ru/auskov/fbkotlin/login/LoginScreen.kt
+++ app/src/main/java/ru/auskov/fbkotlin/login/LoginScreen.kt
@@ -12,17 +12,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.google.firebase.auth.EmailAuthProvider
@@ -34,11 +33,15 @@ import ru.auskov.fbkotlin.components.RoundedButton
 import ru.auskov.fbkotlin.components.RoundedTextInput
 import ru.auskov.fbkotlin.ui.theme.Black10
 import ru.auskov.fbkotlin.ui.theme.Purple40
+import ru.auskov.fbkotlin.ui.theme.Red
+import java.lang.Error
 
 @Composable
 fun LoginScreen() {
     // Initialize Firebase Auth
-    val auth: FirebaseAuth = Firebase.auth
+    val auth: FirebaseAuth = remember {
+        Firebase.auth
+    }
 
     val email = remember {
         mutableStateOf("")
@@ -48,7 +51,11 @@ fun LoginScreen() {
         mutableStateOf("")
     }
 
-    Log.d("MyLog", "User signed in with email: ${auth.currentUser?.email}")
+    val error = remember {
+        mutableStateOf("")
+    }
+
+    //    Log.d("MyLog", "User signed in with email: ${auth.currentUser?.email}")
 
     Image(
         painter = painterResource(R.drawable.login_bg),
@@ -95,7 +102,15 @@ fun LoginScreen() {
             password.value = it
         }
 
-        Spacer(modifier = Modifier.height(50.dp))
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Text(
+            text = error.value,
+            color = Red,
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(modifier = Modifier.height(40.dp))
 
         Column(
             modifier = Modifier
@@ -104,43 +119,75 @@ fun LoginScreen() {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             RoundedButton(name = "Sign In") {
-
+                signIn(
+                    auth,
+                    email.value,
+                    password.value,
+                    onSignInSuccess = {
+                        Log.d("MyLog", "signInWithEmail:success")
+                    },
+                    onSignInError = {e ->
+                        error.value = e
+                    }
+                )
             }
 
             Spacer(modifier = Modifier.height(10.dp))
 
             RoundedButton(name = "Sign Up") {
-
+                signUp(
+                    auth,
+                    email.value,
+                    password.value,
+                    onSignUpSuccess = {
+                        Log.d("MyLog", "signUpWithEmail:success")
+                    },
+                    onSignUpError = { e ->
+                        error.value = e
+                    }
+                )
             }
         }
     }
 }
 
-private fun signUp(auth: FirebaseAuth, email: String, password: String) {
+private fun signUp(
+    auth: FirebaseAuth,
+    email: String,
+    password: String,
+    onSignUpSuccess: () -> Unit,
+    onSignUpError: (String) -> Unit
+) {
+    if (email.isBlank() || password.isBlank()) {
+        return onSignUpError("Email and password cannot be empty")
+    }
+
     auth.createUserWithEmailAndPassword(email, password)
         .addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                // Sign in success, update UI with the signed-in user's information
-                Log.d("MyLog", "createUserWithEmail:success")
-                // val user = auth.currentUser
-            } else {
-                // If sign in fails, display a message to the user.
-                Log.w("MyLog", "createUserWithEmail:failure", task.exception)
-            }
+            if (task.isSuccessful) onSignUpSuccess()
+        }
+        .addOnFailureListener {
+            onSignUpError(it.localizedMessage ?: "Sign Up Error")
         }
 }
 
-private fun signIn(auth: FirebaseAuth, email: String, password: String) {
+private fun signIn(
+    auth: FirebaseAuth,
+    email: String,
+    password: String,
+    onSignInSuccess: () -> Unit,
+    onSignInError: (String) -> Unit
+) {
+    if (email.isBlank() || password.isBlank()) {
+        return onSignInError("Email and password cannot be empty")
+    }
+
     auth.signInWithEmailAndPassword(email, password)
         .addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                // Sign in success, update UI with the signed-in user's information
-                Log.d("MyLog", "signInWithEmail:success")
-                // val user = auth.currentUser
-            } else {
-                // If sign in fails, display a message to the user.
-                Log.w("MyLog", "signInWithEmail:failure", task.exception)
-            }
+            if (task.isSuccessful) onSignInSuccess()
+        }
+        .addOnFailureListener {
+            onSignInError(it.localizedMessage ?: "Sign In Error")
         }
 }
 
