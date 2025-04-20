e676d8fe90f517ede8387e23a7de643d2c7c90d8
andrewuskov <andreuskov2211@yandex.ru>
2025-04-20 13:36:15 +0300

    Lesson 24. Check empty list

--- app/src/main/java/ru/auskov/fbkotlin/main/MainScreen.kt
+++ app/src/main/java/ru/auskov/fbkotlin/main/MainScreen.kt
@@ -1,6 +1,7 @@
 package ru.auskov.fbkotlin.main
 
 import android.util.Log
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,13 +12,16 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.google.firebase.Firebase
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FieldPath
@@ -52,6 +56,10 @@ fun MainScreen(
         mutableStateOf(false)
     }
 
+    val isEmptyListState = remember {
+        mutableStateOf(false)
+    }
+
     val db = remember {
         Firebase.firestore
     }
@@ -59,6 +67,7 @@ fun MainScreen(
     fun getAllBooks(category: String) {
         getFavoriteIdsList(db, navData.uid) { listIds ->
             getBooksList(db, listIds, category) { books ->
+                isEmptyListState.value = books.isEmpty()
                 booksList.value = books
             }
         }
@@ -67,6 +76,7 @@ fun MainScreen(
     fun getFavoritesBooks() {
         getFavoriteIdsList(db, navData.uid) { listIds ->
             getFavoritesBooksList(db, listIds) { books ->
+                isEmptyListState.value = books.isEmpty()
                 booksList.value = books
             }
         }
@@ -133,6 +143,15 @@ fun MainScreen(
                 )
             }
         ) { paddingValue ->
+            if (isEmptyListState.value) {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                )  {
+                    Text("Empty list", color = Color.White)
+                }
+            }
+
             LazyVerticalGrid(
                 columns = GridCells.Fixed(2),
                 modifier = Modifier
@@ -164,6 +183,7 @@ fun MainScreen(
 
                             if (selectedItemState.value == BottomMenuItem.Favourites.title) {
                                 booksList.value = booksList.value.filter {it.isFavorite}
+                                isEmptyListState.value = booksList.value.isEmpty()
                             }
                         }
                     )
@@ -213,18 +233,23 @@ private fun getFavoritesBooksList(
     listIds: List<String>,
     onChangeState: (List<Book>) -> Unit
 ) {
-    db.collection("books")
-        .whereIn(FieldPath.documentId(), listIds)
-        .get()
-        .addOnSuccessListener { task ->
-            val booksList = task.toObjects(Book::class.java).map { book ->
-                book.copy(isFavorite = true)
+    if (listIds.isNotEmpty()) {
+        db.collection("books")
+            .whereIn(FieldPath.documentId(), listIds)
+            .get()
+            .addOnSuccessListener { task ->
+                val booksList = task.toObjects(Book::class.java).map { book ->
+                    book.copy(isFavorite = true)
+                }
+                onChangeState(booksList)
             }
-            onChangeState(booksList)
-        }
-        .addOnFailureListener { error ->
-            Log.d("MyLog", error.message.toString())
-        }
+            .addOnFailureListener { error ->
+                Log.d("MyLog", error.message.toString())
+            }
+    } else {
+        onChangeState(emptyList())
+    }
+
 }
 
 private fun getFavoriteIdsList(
